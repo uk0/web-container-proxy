703c681683cccd1f7efe80aebf5b1d0d2fceee36
zhangjianxin1 <zhang-rd-fe@s139.org>
2018-08-02 13:20:59 +0800

    modify windows file dir

--- servers.go
+++ servers.go
@@ -3,10 +3,15 @@ package web_container_proxy
 // 核心代码  包含静态服务器（文件以及页面） 反向代理
 import (
 	"fmt"
+	"log"
 	"net/http"
 	"net/http/httputil"
 	"net/url"
+	"os"
+	"os/exec"
+	"path/filepath"
 	"regexp"
+	"strings"
 )
 
 func ReverseProxyServer(uri string) http.Handler {
@@ -15,10 +20,20 @@ func ReverseProxyServer(uri string) http.Handler {
 }
 
 func newStaticServer(uri string, hasCustom404 bool, custom404 string) http.Handler {
-	return &StaticServer{http.FileServer(http.Dir(uri)), hasCustom404, custom404}
+	log.Println("newStaticServer is 1" + uri)
+	return &StaticServer{http.FileServer(http.Dir(GetCurrPath() + uri)), hasCustom404, custom404}
+}
+
+func GetCurrPath() string {
+	file, _ := exec.LookPath(os.Args[0])
+	path, _ := filepath.Abs(file)
+	index := strings.LastIndex(path, string(os.PathSeparator))
+	ret := path[:index]
+	return ret
 }
 
 func newSingleFileServer(uri string) http.Handler {
+	log.Println("newSingleFileServer is 2" + uri)
 	return &SingleFileServer{uri}
 }
 
